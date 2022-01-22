738e4dc581a871cae3f6ddbd0d11feaf4d949c77
DP2821 <dhruvilpatelsakariya@gmail.com>
2022-01-22 20:49:50 +0530

    managed section and Faq page improvement

--- HTML/assets/JS/faq.js
+++ HTML/assets/JS/faq.js
@@ -1,3 +1,33 @@
+for(var i = 1; i<9;i++){
+    document.getElementById("pills-home").innerHTML += "<div class=\"forcustomer\">" +
+    "<img class=\"dowm-arrow\" id=\"img"+ i +"\" src=\"./assets/images/right-arrow-grey.png\"" +
+        "onclick=\"fun('p"+ i +"','img"+ i +"')\">" +
+    "<p1 style=\"font-weight: bold;\">" +
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem repudiandae adipisci mollitia?" +
+    "</p1>" +
+    "<div class=\"detail\">" +
+        "<p id=\"p"+ i +"\">" +
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repellendus ratione excepturi rem sit, vitae dolore in nobis porro temporibus expedita officia quia cumque delectus." +
+        "</p>" +
+    "</div>" +
+"</div>"
+}
+
+for(var i = 9; i<19;i++){
+    document.getElementById("pills-profile").innerHTML += "<div class=\"forserviceprovider\">" +
+    "<img class=\"dowm-arrow\" id=\"img"+ i +"\" src=\"./assets/images/right-arrow-grey.png\"" +
+        "onclick=\"fun('p"+ i +"','img"+ i +"')\">" +
+    "<p1 style=\"font-weight: bold;\">" +
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem repudiandae adipisci mollitia?" +
+    "</p1>" +
+    "<div class=\"detail\">" +
+        "<p id=\"p"+ i +"\">" +
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repellendus ratione excepturi rem sit, vitae dolore in nobis porro temporibus expedita officia quia cumque delectus." +
+        "</p>" +
+    "</div>" +
+"</div>"
+}
+
 function fun(nameOfParagraph,img){
     if(document.getElementById(nameOfParagraph).style.display == "block"){
         document.getElementById(img).style.transform = "rotate(360deg)";
