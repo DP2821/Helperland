a7031e37297671836388c2bc050ae8a24d4b8164
DP2821 <dhruvilpatelsakariya@gmail.com>
2022-01-31 21:58:45 +0530

    .NET Project transferred from 3.0 to 6.0

---
+++ Helperland/Helperland/Helperland/Models/MailRequest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Helperland.Models
+{
+    public class MailRequest
+    {
+        public void SendEmail(String receiverEmail, String nameOfUser, String subject, String body)
+        {
+
+            MailMessage mailMessage = new MailMessage("SenderEmail", receiverEmail);
+            mailMessage.Subject = subject;
+            mailMessage.Body = body + "\n\n" + "Your Faithful\n" + nameOfUser;
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.Credentials = new System.Net.NetworkCredential()
+            {
+                UserName = "SenderEmail",
+                Password = "SenderPasword"
+            };
+
+            client.EnableSsl = true;
+            client.Send(mailMessage);
+        }
+    }
+    }
---
+++ Helperland/Helperland/Helperland/Program.cs
@@ -0,0 +1,26 @@
+using Helperland.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<HelperlandContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
