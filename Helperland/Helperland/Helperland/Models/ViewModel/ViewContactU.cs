5b796853699fdf71dbe30125255b7dde2e20948c
DP2821 <dhruvilpatelsakariya@gmail.com>
2022-02-01 17:03:01 +0530

    Field validation using Annotation class is implemented instead of manuall in Contact Us page and Login modal which is opening in same page is changed to redirect on home page and open their

--- Helperland/Helperland/Helperland/Models/ViewModel/ViewContactU.cs
+++ Helperland/Helperland/Helperland/Models/ViewModel/ViewContactU.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,30 @@ namespace Helperland.Models.ViewModel
 {
     public class ViewContactU
     {
+        [Required]
+        [Display(Name = "First name")]
         public String FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
         public String LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Phone number")]
+        [DataType(DataType.PhoneNumber)]
         public String PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
         public String Email { get; set; }
+
+        [Required]
+        [Display(Name = "Subject")]
         public String Subject { get; set; }
+
+        [Required]
+        [Display(Name ="Message")]
         public String Message { get; set; }
     }
 }
